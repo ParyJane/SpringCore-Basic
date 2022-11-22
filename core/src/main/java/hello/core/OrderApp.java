67d9ccd13451470c3982acc60b033ff4efbd0b7a
ParyJane <p_zoo@daum.net>
2022-11-22 21:54:16 +0900

    Spring으로 전환

--- core/src/main/java/hello/core/OrderApp.java
+++ core/src/main/java/hello/core/OrderApp.java
@@ -1,5 +1,8 @@
 package hello.core;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
@@ -9,14 +12,22 @@ import hello.core.order.OrderService;
 public class OrderApp {
 
 	public static void main(String[] args) {
+		/*
+		// AppConfig 사용 버전 
 		AppConfig appConfig = new AppConfig();
 		MemberService memberService = appConfig.memberService();
-		OrderService orderServie = appConfig.orderService();
+		OrderService orderService = appConfig.orderService();
+		*/
+		
+		// Spring 사용 버전 (컨테이너 생성)
+		ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+		MemberService memberService = ac.getBean("memberService", MemberService.class);
+		OrderService orderService = ac.getBean("orderService", OrderService.class);
 		
 		Member member = new Member(1L, "memberA", Grade.VIP);
 		memberService.join(member);
 		
-		Order order = orderServie.createOrder(1L, "itemA", 20000);
+		Order order = orderService.createOrder(1L, "itemA", 20000);
 		System.out.println(order.calculatePrice());
 	}
 
